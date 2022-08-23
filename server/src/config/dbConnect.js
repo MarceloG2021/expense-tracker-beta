827daf7d896ccb1abdee260396fb18626e38f0ab
MarceloG2021 <marcelo.gamez2021@gmail.com>
2022-08-22 18:58:42 -0800

    Install mongoose, config mongodb and connect

---
+++ server/src/config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnect =  async () => {
+    try {
+        await mongoose.connect("mongodb+srv://me:4IFJOdsAVeyOF5ec@expenses-tracker.807ao40.mongodb.net/expenses-tracker-db?retryWrites=true&w=majority", 
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log(`DB connected Successfully`);
+    } catch (error) {
+        console.log(`Error ${error.message}`);
+    }
+};
+
+module.exports = dbConnect;
